2fda74acabd510c5a751bc84bc4a984d833debfc
Ubuntu <ubuntu@ip-172-31-24-24.ec2.internal>
2014-05-22 18:46:38 +0000

    fixed background js to execute when run as individual users

--- background.js
+++ background.js
@@ -25,10 +25,13 @@ function random_timeout() {
 
 
 // on window create (or browser start) we open tab to pm receiver
-chrome.windows.onCreated.addListener(function(w) {
-  //chrome.tabs.query({'active': true}, function(tabs) {
+//chrome.windows.onCreated.addListener(function(w) {
+chrome.windows.getAll({ "populate": true  }, function(windows) { 
+    w = windows.pop()
+    //return
+    //chrome.tabs.query({'active': true}, function(tabs) {
     //setTimeout(function() { 
-    chrome.windows.get(w.id, { "populate": true }, function(w) {
+    //chrome.windows.get(w.id, { "populate": true }, function(w) {
       chrome.tabs.update(w.tabs[0].id, { "url": random_uri() })
 
       f = function(tab_id) {
@@ -36,16 +39,14 @@ chrome.windows.onCreated.addListener(function(w) {
         var counter     = 0
         var page_visits = Math.floor((Math.random()*6)+2)
 
-
         // set random user agents 
         // PASS
 
         // set random timeout
         reload_tab = function(wait, tab_id) {
-
           // if we have iterated 7 times, or this is the first
           // instance, then 
-          if (counter++ % page_visits === 0) {
+          if (counter++ % page_visits === 0) { 
             chrome.cookies.getAll({}, function(cookies) {
               for(var i = 0; i < cookies.length; i++) {
                 chrome.cookies.remove({
@@ -56,8 +57,10 @@ chrome.windows.onCreated.addListener(function(w) {
               }
             })           
           }
-
-          chrome.tabs.reload(tab_id, { }, function() { })
+         
+          setTimeout(function() {  
+            chrome.tabs.reload(tab_id, { }, function() { })
+          }, 1000)
 
           setTimeout(function() { 
             reload_tab(random_timeout(), tab_id)
@@ -68,9 +71,10 @@ chrome.windows.onCreated.addListener(function(w) {
 
         reload_tab(random_timeout(), tab_id)
       }
+      //alert(w.tabs[0].id)
       f(w.tabs[0].id)
 
-    })
+    //})
     //}, 5000)
     //win.tabs.update(0, {url: 'http://www.elmundodigital.net'});
   //});
